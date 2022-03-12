78f59778a2941fe2900cd629d1e0f130aefcd403
Ryan Tankeng <92751701+MrRyanWise@users.noreply.github.com>
2022-03-12 09:53:08 +0100

    Add files via upload

---
+++ JS/orders.js
@@ -0,0 +1,28 @@
+const Orders = [
+    {
+        productName:'Foldable Mini Drone',
+        productNumber:'85631',
+        paymentStatus:'Due',
+        shipping:'Rending'
+    }, {
+        productName:'Foldable Mini Drone',
+        productNumber:'85631',
+        paymentStatus:'Due',
+        shipping:'declined'
+    }, {
+        productName:'Foldable Mini Drone',
+        productNumber:'85631',
+        paymentStatus:'Due',
+        shipping:'Delivred'
+    }, {
+        productName:'Foldable Mini Drone',
+        productNumber:'85631',
+        paymentStatus:'Due',
+        shipping:'Rending'
+    }, {
+        productName:'Foldable Mini Drone',
+        productNumber:'85631',
+        paymentStatus:'Due',
+        shipping:'Pending'
+    }
+]
